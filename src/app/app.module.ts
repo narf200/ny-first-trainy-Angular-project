540d642c000f1123df26c76cb27cba58af4d90b4
narf200 <narf200@gmail.com>
2023-04-12 16:06:00 +0200

    init: start my-first-Angular-project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from "./server/server.component";
+import {FormsModule} from "@angular/forms";
+import { ServersComponent } from './servers/servers.component';
+import {WarningAlertComponent} from "./warningAlert/warningAlert.component";
+import {SuccessAlertComponent} from "./successAlert/successAlert.component";
+import { UserComponent } from './user/user.component';
+import { PasswordComponent } from './password/password.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+    UserComponent,
+    PasswordComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
